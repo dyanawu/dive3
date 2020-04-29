870f78cf6f47ea5e8c83be9a3303f3b206d0e2e8
Dyana Wu <dyanawu@gmail.com>
2020-04-29 18:41:49 +0800

    Add routes for dives

--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,21 @@ const router = express.Router();
 
 // require the controllers which have  a functon for each route
 const Trips = require('../controllers/trips');
+const Dives = require('../controllers/dives');
 
 router.get('/', (req, res) => {
   res.render('content');
 });
 
-router.get('/trips', Trips.list);
+router.get('/trips', Trips.listTrips);
 
 router.get('/trip/new', Trips.showForm);
 
 router.post('/trip/new', Trips.add);
+
+router.get('/trip/:id', Dives.listDivesByTrip);
+
+router.get('/dives', Dives.listDives);
 
 router.get('/login', (req, res) => {
   res.render('login');

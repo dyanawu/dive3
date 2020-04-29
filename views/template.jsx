b384d5f50bdf003e8a6597e8d63df8d81822d970
Dyana Wu <dyanawu@gmail.com>
2020-04-29 18:38:29 +0800

    Add mobile viewport

--- views/template.jsx
+++ views/template.jsx
@@ -12,6 +12,7 @@ class Template extends React.Component {
     return (
       <html className="h-100">
         <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="stylesheet" href="/css/bootstrap.min.css" />
           <link rel="stylesheet" href="/css/fontawesome/css/all.min.css" />
           <link rel="stylesheet" href="/css/bootstrap-datepicker.min.css" />

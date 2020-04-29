1291bcb0db6c5c789ade3fe72b5b31aeb1f62a19
Dyana Wu <dyanawu@gmail.com>
2020-04-29 11:45:00 +0800

    Add trip routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // require the controllers which have  a functon for each route
-const controller = require('../controllers/generic_c');
+const Trips = require('../controllers/trips');
 
 router.get('/', (req, res) => {
   res.render('content');
 });
+
+router.get('/trips', Trips.list);
+
+router.get('/trip/new', Trips.add);
 
 router.get('/login', (req, res) => {
   res.render('login');

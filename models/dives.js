239d71747994b0108f8e46596c4c4df8fb179db9
Dyana Wu <dyanawu@gmail.com>
2020-05-02 02:46:47 +0800

    Add dive model functions

--- models/dives.js
+++ models/dives.js
@@ -26,6 +26,25 @@ module.exports = class Dive {
     let value = [trip_id];
 
     return await dbpool.query(query, value);
+  }
+
+  static async getLastDiveNo() {
+    let query =
+        'SELECT dive_no FROM dives ' +
+        'WHERE id = (SELECT MAX(id) from dives)';
+
+    return await dbpool.query(query);
+  };
+
+  static async getDiveById(dive_id) {
+    let query =
+        'SELECT dives.*, trip.name as tripname FROM dives ' +
+        'INNER JOIN trips ' +
+        'ON (dives.trip_id = trips.id) ' +
+        'WHERE dives.id = $1';
+    let value = [dive_id];
+
+    return await dbpool.query(query, value);
   };
 
 };
